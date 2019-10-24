d8b2a1ab8cad7ef7fbf5a57774a4278659df0f25
wcheung530 <wes.cheung530@gmail.com>
2019-10-23 20:38:34 -0400

    implemented rook class

--- Chess/src/Chess/Rook.java
+++ Chess/src/Chess/Rook.java
@@ -1,8 +1,15 @@
 package Chess;
 
+/**
+ * The Rook class is an extension of the Piece class and creates a Rook Piece.
+ * @author Wesley Cheung
+ * @author Dennis Yu
+ *
+ */
+
 public class Rook extends Piece {
 	public char team;
-	public String type = "Pawn";
+	public String type = "Rook";
 	boolean firstM = true;
 	
 	public Rook(char team) {
@@ -14,11 +21,107 @@ public class Rook extends Piece {
 	public String getType() {
 		return this.type;
 	}
-	
+	/**
+	 * check() takes in the game board, current row, current column, new row, and new column as parameters and returns true if new position is a valid move for Rook
+	 * 
+	 * @param gameBoard is the game board
+	 * @param x1 is the current row
+	 * @param y1 is the current column
+	 * @param x2 is the new row
+	 * @param y2 is the new column
+	 * 
+	 * @return true if move is valid or false if not
+	 */
 	public boolean check(Piece gameBoard[][], 
 			int x1, int y1, 
 			int x2, int y2) {
-		return true; 
+		
+		//check if valid move for a Rook
+		if((x1 == x2 || y1 == y2 ) && !((x1 == x2) && (y1 == y2))) {
+			
+			//check if new position is empty:
+			if(gameBoard[x2][y2] == null) {
+				
+				if(isPathEmpty(gameBoard, x1, y1, x2, y2)) {
+					return true;
+				}
+				
+				else {
+					return false;
+				}
+			}
+			
+			//new position is not empty
+			else {
+				
+				//checks team of both pieces
+				if(gameBoard[x1][y1].getTeam() == gameBoard[x2][y2].getTeam()) {
+					return false;
+				}
+				
+				else {
+					if(isPathEmpty(gameBoard, x1, y1, x2, y2)) {
+						return true;
+					}
+					
+					else {
+						return false;
+					}
+				}
+			}
+		}
+		
+		else {
+			return false;
+		}
+	}
+	
+	/**
+	 * isPathEmpty() checks if the path is clear for the Rook to move from its current position to the new position
+	 * @param gameBoard
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * 
+	 * @return true if the path is clear otherwise false
+	 */
+	private boolean isPathEmpty(Piece gameBoard[][], int x1, int y1, int x2, int y2) {
+		int offset;
+		
+		if(x1 != x2){
+			if(x1 < x2){
+				offset = 1;
+			}else{
+				offset = -1;
+			}
+			
+			for(int x = x1 + offset; x != x2; x += offset){
+				//Go from currentRow to newRow, and check every space
+				if(gameBoard[x][y1] != null){
+					return false;
+				}
+			}
+		}
+		
+		//Now do the same for columns
+		if(y1 != y2){
+			if(y1 < y2){
+				offset = 1;
+			}else{
+				offset = -1;
+			}
+			
+			for(int y = y1 + offset; y != y2; y += offset){
+				
+				//Go from currentCol to newCol, and check every space
+				if(gameBoard[x1][y] != null){
+					return false;
+				}
+			}
+		}
+		
+		return true;
 	}
 	
 }
