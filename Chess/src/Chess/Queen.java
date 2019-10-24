e5578e6fce95d7b71f277249fd0e07342fee4e12
wcheung530 <wes.cheung530@gmail.com>
2019-10-24 00:37:47 -0400

    implemented Queen class

--- Chess/src/Chess/Queen.java
+++ Chess/src/Chess/Queen.java
@@ -18,6 +18,8 @@ public class Queen extends Piece {
 	public boolean check(Piece gameBoard[][], 
 			int x1, int y1, 
 			int x2, int y2) {
-		return true; 
+		
+		return new Rook(team).check(gameBoard, x1, y2, x2, y2) || new Bishop(team).check(gameBoard, x1, y1, x2, y2);
+		
 	}
 }
