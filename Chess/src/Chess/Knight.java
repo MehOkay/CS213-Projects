71e7f65a3d307b792a14a216681b4ef917d2506c
wcheung530 <wes.cheung530@gmail.com>
2019-10-25 01:41:58 -0400

    implemented knight class

--- Chess/src/Chess/Knight.java
+++ Chess/src/Chess/Knight.java
@@ -17,7 +17,47 @@ public class Knight extends Piece {
 	public boolean check(Piece gameBoard[][], 
 			int x1, int y1, 
 			int x2, int y2) {
-		return true; 
+		
+		if(checkHelper(gameBoard, x1, y1, x2, y2) == true) {
+		/*is a valid Knight move*/
+			
+			//check if new position is empty:
+			if(gameBoard[x2][y2] == null) {
+				return true;
+			}
+			
+			//new position is not empty
+			else {
+				
+				//checks team of both pieces
+				if(gameBoard[x1][y1].getTeam() == gameBoard[x2][y2].getTeam()) {
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+		}
+		
+		else {
+		//not a valid Knight move
+			return false;
+		}
+	}
+	
+	private boolean checkHelper(Piece gameBoard[][], 
+			int x1, int y1, 
+			int x2, int y2) {
+		
+		if(Math.abs(x2 - x1) == 2 && Math.abs(y2 - y1) == 1){
+			return true;
+		}
+		
+		if(Math.abs(x2 - x1) == 1 && Math.abs(y2 - y1) == 2){
+			return true;
+		}
+		
+		return false;
 	}
 	
 }
