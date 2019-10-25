aa64cf1b9e2d44c2ff06e927a2450f83d7c56faa
wcheung530 <wes.cheung530@gmail.com>
2019-10-25 13:54:24 -0400

    added documentation to knight class

--- Chess/src/Chess/Knight.java
+++ Chess/src/Chess/Knight.java
@@ -1,5 +1,11 @@
 package Chess;
 
+/**
+ * The Knight class is an extension of the Piece class and creates a Knight Piece.
+ * @author Wesley Cheung
+ * @author Dennis Yu
+ * 
+ */
 public class Knight extends Piece {
 	public char team;
 	public String type = "Knight";
@@ -14,11 +20,23 @@ public class Knight extends Piece {
 		return this.type;
 	}
 	
+	/**
+	 * checMove() takes in the current position and new position of knight's move and returns true if it is a valid move for Knight. 
+	 * This is different from checkHelper() which determines ALL possible knight moves. 
+	 * 
+	 * @param gameBoard
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * 
+	 * @return true if move is valid and false otherwise.
+	 */
 	public boolean checkMove(Piece gameBoard[][], 
 			int x1, int y1, 
 			int x2, int y2) {
 		
-		if(checkHelper(gameBoard, x1, y1, x2, y2) == true) {
+		if(checkMoveHelper(gameBoard, x1, y1, x2, y2) == true) {
 		/*is a valid Knight move*/
 			
 			//check if new position is empty:
@@ -39,13 +57,19 @@ public class Knight extends Piece {
 			}
 		}
 		
-		else {
-		//not a valid Knight move
-			return false;
-		}
+		return false;
 	}
 	
-	private boolean checkHelper(Piece gameBoard[][], 
+	/**
+	 * helper method to checkMove(). Checks to see if destination is one of the possible new positions of a knight.
+	 * @param gameBoard
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return
+	 */
+	private boolean checkMoveHelper(Piece gameBoard[][], 
 			int x1, int y1, 
 			int x2, int y2) {
 		
