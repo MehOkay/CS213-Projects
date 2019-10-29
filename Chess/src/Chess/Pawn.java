5788c747a47ddad34ce0bc55478571b6931d906c
Giga23 <dy189@scarletmail.rutgers.edu>
2019-10-29 16:09:14 -0400

    en passant now works

--- Chess/src/Chess/Pawn.java
+++ Chess/src/Chess/Pawn.java
@@ -27,6 +27,11 @@ public class Pawn extends Piece {
 	public void setPas() {
 		enPass = false;
 	}
+	/*
+	public boolean checkMove(Piece gameBoard[][], int x1, int y1, int x2, int y2) {
+		System.out.println("pawn");
+		return true;
+	}*/
 	
 	/**
 	 * checkMove() takes in the game board, current row, current column, new row, 
@@ -40,55 +45,129 @@ public class Pawn extends Piece {
 	 * 
 	 * @return true if move is valid or false if not
 	 */
-	public boolean checkMove(Piece gameBoard[][], int x1, int y1, 
-			int x2, int y2) {
-		
+	
+	public boolean checkMove(Piece gameBoard[][], int x1, int y1, int x2, int y2) {
+	
 		//pawns cannot go backward
-		if(gameBoard[x1][y1].getTeam() == 'w') {
-			if(x1 > x2)
+		if(gameBoard[x1][y1].getTeam() == 'b') {
+			if(x1 > x2) {
+				System.out.print(1);
 				return false;
+				}
 		}
 		else{
-			if(x2 > x1)
+			if(x2 > x1) {
+				System.out.println(2);
 				return false;
+			}
 		}
-		
 		//white 
 		if(gameBoard[x1][y1].getTeam() == 'w') {
-			
+			//move
+			if(y1 == y2) {
+				//Destination is open
+				if(gameBoard[x2][y2] == null) {
+					//two forward
+					if(x1 - x2 == 2) {
+						//can only move 2 from first row
+						if(x1 == 6) {
+							//nothing in between
+							if(gameBoard[x2+1][y2] == null) {
+								this.enPass = true;
+								return true;
+							}
+							else {System.out.print("w2");
+							return false;}
+						}
+						else {System.out.print("w3");
+						return false;}
+					}
+					//1 forward
+					else if(x1 - x2 == 1) {
+						this.setPas();
+						return true;
+					}
+					//more than 2 forward
+					else {System.out.print("w4");
+					return false;}
+				}
+				//path blocked
+				else {System.out.print("w5");
+				return false;}
+			}
+			//kill 
+			else {
+				//pawns can only kill 1 diagonal away
+				if(Math.abs(y1-y2) == 1 && Math.abs(x1-x2) == 1) {
+					//En passant
+					/*
+					if(gameBoard[x2][y2] == null) {
+						Pawn pawn = (Pawn)gameBoard[x1][y2];
+						if(pawn.getPas()) {
+							pawn.setPas();
+							gameBoard[x2][y2] = pawn;
+							gameBoard[x1][y1] = null;
+							return true;
+						}
+						else {
+							{System.out.print("w6");
+							return false;}
+						}
+					
+					}
+					*/
+					//normal kill
+					if(gameBoard[x2][y2] != null && gameBoard[x2][y2].getTeam() == 'b') {
+						this.setPas();
+						return true;
+					}
+					else {System.out.print("w7");
+					return false;}
+				}
+			}
+			System.out.print("w8");
+			return false;
+		}
+		
+		//black
+		else{
 			//move
 			if(y1 == y2) {
 				//Destination is open
 				if(gameBoard[x2][y2] == null) {
 					//two forward
 					if(x1 - x2 == -2) {
-						//can only move 2 from first row
-						if(x1 == 6) {
+						//first row
+						if(x1 == 1) {
 							//nothing in between
 							if(gameBoard[x2-1][y2] == null) {
 								this.enPass = true;
 								return true;
 							}
-							else return false;
+							else {System.out.print("b2");
+							return false;}
 						}
-						else return false;
+						else {System.out.print("b3");
+						return false;}
 					}
-					//1 forward
 					else if(x1 - x2 == -1) {
+						this.setPas();
 						return true;
 					}
 					//more than 2 forward
-					else
-						return false;
+					else{System.out.print("b4");
+					return false;}
 				}
 				//path blocked
-				else return false;
+				else {System.out.print("b5");
+				return false;}
 			}
 			//kill 
 			else {
 				//pawns can only kill 1 diagonal away
 				if(Math.abs(y1-y2) == 1 && Math.abs(x1-x2) == 1) {
 					//En passant
+					/*
 					if(gameBoard[x2][y2] == null) {
 						Pawn pawn = (Pawn)gameBoard[x1][y2];
 						if(pawn.getPas()) {
@@ -98,76 +177,25 @@ public class Pawn extends Piece {
 							return true;
 						}
 						else {
-							return false;
+							{System.out.print("b6");
+							return false;}
 						}
-			
+					
 					}
-					//normal kill
-					if(gameBoard[x2][y2] != null && gameBoard[x2][y2].getTeam() == 'b') {
-						return true;
-					}
-					else 
-						return false;
-				}
-			}
-			return false;
-		}
-		//black
-		else{
-			if(y1 == y2) {
-				//Destination is open
-				if(gameBoard[x2][y2] == null) {
-					//two forward
-					if(x1 - x2 == 2) {
-						//first row
-						if(x1 == 1) {
-							//nothing in between
-							if(gameBoard[x2+1][y2] == null) {
-								this.enPass = true;
-								return true;
-							}
-							else return false;
-						}
-						else return false;
-					}
-					else if(x1 - x2 == 1) {
-						return true;
-					}
-					//more than 2 forward
-					else
-						return false;
-				}
-				//path blocked
-				else return false;
-			}
-			//kill 
-			else {
-				//pawns can only kill 1 diagonal away
-				if(Math.abs(y1-y2) == 1 && Math.abs(x1-x2) == 1) {
-					//En passant
-					if(gameBoard[x2][y2] == null) {
-						Pawn pawn = (Pawn)gameBoard[x1][y2];
-						if(pawn.getPas()) {
-							pawn.setPas();
-							gameBoard[x2][y2] = pawn;
-							gameBoard[x1][y1] = null;
-							return true;
-						}
-						else {
-							return false;
-						}
-			
-					}
+					*/
 					//normal kill
 					if(gameBoard[x2][y2] != null && gameBoard[x2][y2].getTeam() == 'w') {
+						this.setPas();
 						return true;
 					}
-					else 
-						return false;
+					else {System.out.print("b7");
+					return false;}
 				}
 			}
 		}
+		System.out.println("end");
 		return false;
 	}
+	
 }
 
