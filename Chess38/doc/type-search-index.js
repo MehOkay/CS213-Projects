594720f9cdc3588d9ea44fb9536d01ff7e2f895b
wcheung530 <wes.cheung530@gmail.com>
2019-10-30 19:23:07 -0400

    finished javadoc comments and generated the html file

--- Chess38/doc/type-search-index.js
+++ Chess38/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Chess","l":"Bishop"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Chess","l":"Piece"}]
--- Chess38/src/Chess/Piece.java
+++ Chess38/src/Chess/Piece.java
@@ -14,6 +14,14 @@ public abstract class Piece {
 	public abstract String getType();
 	/**
 	 * Each subclass of Piece implements their own version of checkMove
+	 * @param gameBoard - the game board that holds the piece
+	 * @param x1 - the current row of the piece
+	 * @param y1 - the current column of the piece
+	 * @param x2 - the new row of the piece
+	 * @param y2 - the new column of the piece
+	 * 
+	 *  @return boolean value if the new row and new column is a valid move for the piece
+	 * 
 	 */
 	public abstract boolean checkMove(Piece gameBoard[][], 
 			int x1, int y1, 
